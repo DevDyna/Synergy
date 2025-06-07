d15e53e38d902373348b144cc1ab7a0f60a408d1
DevDyna <andrearisorti2003@gmail.com>
2025-06-07 15:25:16 +0200

    updated loot table provider

--- src/main/java/com/devdyna/synergy/datagen/Controller.java
+++ src/main/java/com/devdyna/synergy/datagen/Controller.java
@@ -2,6 +2,8 @@ package com.devdyna.synergy.datagen;
 
 import static com.devdyna.synergy.Main.ID;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import com.devdyna.synergy.datagen.client.DataBlockModelState;
@@ -11,11 +13,12 @@ import com.devdyna.synergy.datagen.server.DataBlockTag;
 import com.devdyna.synergy.datagen.server.DataItemTag;
 import com.devdyna.synergy.datagen.server.DataLoot;
 import com.devdyna.synergy.datagen.server.DataRecipe;
-
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -40,7 +43,8 @@ public class Controller {
         DataBlockTag blocktag = new DataBlockTag(po, pr, f);
         providerGen(e, g, blocktag);
         providerGen(e, g, new DataItemTag(po, pr, blocktag.contentsGetter()));
-        providerGen(e, g, new DataLoot(po, pr));
+        providerGen(e, g, new LootTableProvider(po, Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(DataLoot::new, LootContextParamSets.BLOCK)), pr));
         providerGen(e, g, new DataRecipe(po, pr));
 
     }
