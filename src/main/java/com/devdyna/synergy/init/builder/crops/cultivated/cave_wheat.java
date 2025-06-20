b4ba3d9da27fa4aacdb5063652418600eeed1dea
DevDyna <andrearisorti2003@gmail.com>
2025-06-21 01:20:21 +0200

    placement cave wheat cultivated

--- src/main/java/com/devdyna/synergy/init/builder/crops/cultivated/cave_wheat.java
+++ src/main/java/com/devdyna/synergy/init/builder/crops/cultivated/cave_wheat.java
@@ -3,8 +3,14 @@ package com.devdyna.synergy.init.builder.crops.cultivated;
 import com.devdyna.synergy.init.builder._core.crops.BaseShortCropBlock;
 import com.devdyna.synergy.init.types.zItems;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
 
+@SuppressWarnings("null")
 public class cave_wheat extends BaseShortCropBlock {
 
     public cave_wheat() {
@@ -14,6 +20,18 @@ public class cave_wheat extends BaseShortCropBlock {
     @Override
     protected ItemLike getBaseSeedId() {
         return zItems.CAVE_WHEAT_SEEDS.get();
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return level.getBlockState(pos.below()).is(BlockTags.BASE_STONE_OVERWORLD)
+                || level.getBlockState(pos.below()).is(BlockTags.DIRT);
+    }
+
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        return level.getBlockState(pos.below()).is(BlockTags.BASE_STONE_OVERWORLD) ? true
+                : super.mayPlaceOn(state, level, pos);
     }
 
 }
