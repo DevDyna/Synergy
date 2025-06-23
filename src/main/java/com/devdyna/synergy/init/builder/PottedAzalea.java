6e702de8f90e16821a776e3df3cfba60f0faf5b3
DevDyna <andrearisorti2003@gmail.com>
2025-06-23 16:58:12 +0200

    Update PottedAzalea.java

--- src/main/java/com/devdyna/synergy/init/builder/PottedAzalea.java
+++ src/main/java/com/devdyna/synergy/init/builder/PottedAzalea.java
@@ -1,33 +1,24 @@
 package com.devdyna.synergy.init.builder;
 
-import com.devdyna.synergy.utils.LevelUtil;
+import java.util.List;
+
+import com.devdyna.synergy.init.builder._core.crops.BaseCropBlock;
+import com.devdyna.synergy.init.types.zItems;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.material.PushReaction;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("null")
-public class PottedAzalea extends Block {
-
-    public static final IntegerProperty AGE = BlockStateProperties.AGE_7;
+public class PottedAzalea extends BaseCropBlock {
 
     protected static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[] {
             Block.box(5.0, 0.0, 5.0, 11.0, 12.0, 11.0),
@@ -41,54 +32,38 @@ public class PottedAzalea extends Block {
 
     };
 
+    public PottedAzalea() {
+        super(Properties.of());
+    }
+
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE_BY_AGE[state.getValue(AGE)];
     }
 
-    public PottedAzalea() {
-        super(BlockBehaviour.Properties.of().randomTicks().sound(SoundType.FLOWERING_AZALEA)
-                .instabreak().noOcclusion()
-                .pushReaction(PushReaction.DESTROY));
-        this.registerDefaultState(stateDefinition.any()
-                .setValue(AGE, 0));
-    }
+    @Override
+    public List<ItemStack> getItemResult(Level level, BlockState state, BlockPos pos, Player player, ItemStack tool) {
 
-    public BlockState getStateForAge(int age) {
-        return this.defaultBlockState().setValue(AGE, Integer.valueOf(age));
-    }
-
-    protected boolean isRandomlyTicking(BlockState state) {
-        return state.getValue(AGE) < 7;
-    }
-
-    protected void randomTick(BlockState state, ServerLevel world, BlockPos pos, RandomSource random) {
-        int stage = state.getValue(AGE);
-        if (stage < 7) {
-            world.setBlock(pos, getStateForAge(stage + 1), 2);
-        }
+        // TODO convert removing pot
+        return List.of(
+                new ItemStack(zItems.AZALEA_SEEDS, level.random.nextInt(2)),
+                new ItemStack(zItems.SMALL_AZALEA_LEAF, level.random.nextInt(3)+1),
+                new ItemStack(zItems.SMALL_AZALEA_ROOTS, level.random.nextInt(1)+1));
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(AGE);
+    protected ItemLike getBaseSeedId() {
+        return zItems.AZALEA_SEEDS.get();
     }
 
     @Override
-    protected ItemInteractionResult useItemOn(ItemStack i, BlockState b, Level l, BlockPos p,
-            Player e, InteractionHand h, BlockHitResult r) {
-        if (!l.isClientSide)
-            if (b.getValue(AGE) >= 6) {
-                l.setBlockAndUpdate(p, b.setValue(AGE, LevelUtil.getRandomValue(4, l)));
-                var items = LevelUtil.getItemStackFromLootTable(l, b);
-                items.forEach(s -> {
-                    if (!s.is(Blocks.FLOWER_POT.asItem()))
-                        LevelUtil.popItemFromPos(l, p.above(), s);
-                });
-                return ItemInteractionResult.SUCCESS;
-            }
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return true;
+    }
 
-        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        return false;
     }
 
 }
