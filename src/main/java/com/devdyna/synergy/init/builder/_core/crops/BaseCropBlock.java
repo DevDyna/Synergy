436abb68a687784ee9850d7383642dd347e8bf09
DevDyna <andrearisorti2003@gmail.com>
2025-06-23 16:58:08 +0200

    harvestable

--- src/main/java/com/devdyna/synergy/init/builder/_core/crops/BaseCropBlock.java
+++ src/main/java/com/devdyna/synergy/init/builder/_core/crops/BaseCropBlock.java
@@ -2,36 +2,24 @@ package com.devdyna.synergy.init.builder._core.crops;
 
 import java.util.List;
 
-import javax.annotation.Nonnull;
-
-import com.devdyna.synergy.utils.LevelUtil;
-
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder.Reference;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.BlockHitResult;
-import net.neoforged.neoforge.capabilities.Capabilities.FluidHandler;
-import net.neoforged.neoforge.capabilities.Capabilities.ItemHandler;
-import net.neoforged.neoforge.fluids.capability.templates.FluidHandlerItemStack;
-import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 @SuppressWarnings("null")
-public class BaseCropBlock extends CropBlock {
+public class BaseCropBlock extends CropBlock implements Harvestable {
 
     public BaseCropBlock(Properties properties) {
         super(properties.mapColor(MapColor.PLANT)
@@ -46,16 +34,31 @@ public class BaseCropBlock extends CropBlock {
     protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos,
             Player player, InteractionHand hand, BlockHitResult hitResult) {
 
-        if (!level.isClientSide && isMaxAge(state)) {
-            List<ItemStack> drops = Block.getDrops(state, (ServerLevel) level, pos, null, player, stack);
+        if (harvestCrop(level, state, pos, player, stack))
+            return ItemInteractionResult.SUCCESS;
+        else
 
-            drops.forEach(item -> ItemHandlerHelper.giveItemToPlayer(player, item));
+            return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
+    }
 
-            level.setBlockAndUpdate(pos,
-                    this.defaultBlockState().setValue(getAgeProperty(), LevelUtil.getRandomValue(getMaxAge() - 2, level)));
-        }
+    @Override
+    public List<ItemStack> getItemResult(Level level, BlockState state, BlockPos pos, Player player, ItemStack tool) {
+        return Block.getDrops(state, (ServerLevel) level, pos, null, player, tool);
+    }
 
-        return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
+    @Override
+    public int maxAge() {
+        return getMaxAge();
+    }
+
+    @Override
+    public boolean canBeHarvested(BlockState state) {
+        return isMaxAge(state);
+    }
+
+    @Override
+    public IntegerProperty property() {
+        return getAgeProperty();
     }
 
 }
