29917afd285d50ba9e1289a8096956d12530d8a4
DevDyna <andrearisorti2003@gmail.com>
2025-06-23 23:15:22 +0200

    Update wild_rice.java

--- src/main/java/com/devdyna/synergy/init/builder/crops/wild/wild_rice.java
+++ src/main/java/com/devdyna/synergy/init/builder/crops/wild/wild_rice.java
@@ -6,31 +6,16 @@ import com.devdyna.synergy.Main;
 import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.init.builder._core.crops.BaseWildCropBlock;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 
 @SuppressWarnings("null")
 public class wild_rice extends BaseWildCropBlock {
 
     public wild_rice() {
         super(Properties.of());
-    }
-
-    @Override
-    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-        return level.getBlockState(pos.below()).is(Blocks.WATER) ? true : super.canSurvive(state, level, pos);
-    }
-
-    @Override
-    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
-        return level.getBlockState(pos.below()).is(Blocks.WATER) ? true : super.mayPlaceOn(state, level, pos);
     }
 
     @Override
