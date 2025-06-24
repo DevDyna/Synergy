66402e95c2a7449e648a5c9586a2996aa0a0c486
DevDyna <andrearisorti2003@gmail.com>
2025-06-24 11:51:15 +0200

    fixed refoactorizer

--- src/main/java/com/devdyna/synergy/init/builder/pipeTools/PipeRefactorizer.java
+++ src/main/java/com/devdyna/synergy/init/builder/pipeTools/PipeRefactorizer.java
@@ -2,13 +2,12 @@ package com.devdyna.synergy.init.builder.pipeTools;
 
 import java.util.List;
 
-import com.devdyna.synergy.init.builder._core.pipes.pipeProperties;
+import com.devdyna.synergy.init.builder._core.pipes.nodeType;
 import com.devdyna.synergy.init.builder._core.pipes.pipeType;
 import com.devdyna.synergy.init.types.zBlockTag;
 import com.devdyna.synergy.Main;
 import com.devdyna.synergy.zStatic;
 
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionResult;
@@ -16,7 +15,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
-import net.neoforged.neoforge.capabilities.Capabilities;
 
 @SuppressWarnings("null")
 public class PipeRefactorizer extends Item {
@@ -25,49 +23,25 @@ public class PipeRefactorizer extends Item {
         super(new Properties().stacksTo(1));
     }
 
-    // TODO dont work
     @Override
     public InteractionResult useOn(UseOnContext c) {
         var level = c.getLevel();
         var pos = c.getClickedPos();
         var state = level.getBlockState(pos);
         var player = c.getPlayer();
-        if (state.is(zBlockTag.PIPE_CONNECTORS)) {
+        if (state.is(zBlockTag.CAN_CONNECT)) {
 
             player.playSound(SoundEvents.CHERRY_WOOD_BUTTON_CLICK_ON);
 
             player.swing(player.getUsedItemHand());
 
-            for (Direction face : pipeType.DIRECTIONS) {
+            if (state.is(zBlockTag.PIPE))
+                state = pipeType.updatePipeOnPlace(state, level, pos);
 
-                var offset = level.getBlockState(pos.relative(face));
-                if (offset.is(zBlockTag.PIPE_CONNECTORS)) {
-                    // TIP. default is true
+            if (state.is(zBlockTag.NODE))
+                state = nodeType.updateNodeOnPlace(state, level, pos, state.getValue(nodeType.FACING));
 
-                    // connect to another pipe connector
-                    if (offset.getValue(pipeType.PROPRTIES
-                            .get(pipeType.DIRECTIONS.indexOf(face.getOpposite()))) != pipeProperties.NODE)
-                        level.setBlockAndUpdate(pos.relative(face),
-                                offset = offset.setValue(
-                                        pipeType.PROPRTIES.get(pipeType.DIRECTIONS.indexOf(face.getOpposite())),
-                                        pipeProperties.TRUE));
-                } else {
-
-                    // connect to BE itemhandler
-                    if (level.getBlockEntity(pos.relative(face)) != null
-                            && Capabilities.ItemHandler.BLOCK.getCapability(level, pos.relative(face),
-                                    level.getBlockState(pos.relative(face)),
-                                    level.getBlockEntity(pos.relative(face)), face.getOpposite()) != null) {
-                        state = state.setValue(pipeType.PROPRTIES.get(pipeType.DIRECTIONS.indexOf(face)),
-                                pipeProperties.OUTPUT);
-                    } else {
-                        // remove connection
-
-                        state = state.setValue(pipeType.PROPRTIES.get(pipeType.DIRECTIONS.indexOf(face)),
-                                pipeProperties.FALSE);
-                    }
-                }
-            }
+            level.setBlockAndUpdate(pos, state);
 
             return InteractionResult.SUCCESS;
         }
