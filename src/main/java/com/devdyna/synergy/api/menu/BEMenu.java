c7a64dbc10fa26986bd885484d71297e538f23c7
DevDyna <andrearisorti2003@gmail.com>
2025-08-26 22:22:34 +0200

    new api for be with gui

---
+++ src/main/java/com/devdyna/synergy/api/menu/BEMenu.java
@@ -0,0 +1,79 @@
+package com.devdyna.synergy.api.menu;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.synergy.api.beLogic.ItemStorageBlock;
+import com.devdyna.synergy.api.coreBE.BaseBE;
+import com.devdyna.synergy.init.types.zHandlers;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Containers;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.ItemStackHandler;
+
+@SuppressWarnings("null")
+public abstract class BEMenu extends BaseBE implements ItemStorageBlock {
+
+    public BEMenu(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
+        super(type, pos, blockState);
+    }
+
+    public void clearContents() {
+        getStorage().setStackInSlot(0, ItemStack.EMPTY);
+    }
+
+    public void drops() {
+        SimpleContainer inv = new SimpleContainer(getStorage().getSlots());
+        for (int i = 0; i < getStorage().getSlots(); i++)
+            inv.setItem(i, getStorage().getStackInSlot(i));
+        Containers.dropContents(this.level, this.worldPosition, inv);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+        tag.put("inventory", getStorage().serializeNBT(registries));
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+        getStorage().deserializeNBT(registries, tag.getCompound("inventory"));
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+        return saveWithoutMetadata(pRegistries);
+    }
+
+    @Override
+    public ItemStackHandler getStorage() {
+        return getData(zHandlers.ITEM_STORAGE);
+    }
+
+    @Override
+    public int MachineSlots() {
+        return 1;
+    }
+
+        @Override
+    public Component getContainerName() {
+        return Component.translatable(this.getBlockState().getBlock().getDescriptionId());
+    }
+
+}
--- src/main/java/com/devdyna/synergy/init/builder/reactor/port/ReactorPortBlock.java
+++ src/main/java/com/devdyna/synergy/init/builder/reactor/port/ReactorPortBlock.java
@@ -1,33 +1,28 @@
 package com.devdyna.synergy.init.builder.reactor.port;
 
 import java.util.List;
+import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
 import com.devdyna.synergy.Main;
 import com.devdyna.synergy.zStatic;
-import com.devdyna.synergy.api.coreBE.BaseBlockBE;
-import com.mojang.serialization.MapCodec;
-
+import com.devdyna.synergy.api.menu.BlockMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.phys.BlockHitResult;
 
 @SuppressWarnings("null")
-public class ReactorPortBlock extends BaseBlockBE {
-
-    public static final MapCodec<ReactorPortBlock> CODEC = simpleCodec(ReactorPortBlock::new);
+public class ReactorPortBlock extends BlockMenu {
 
     public ReactorPortBlock() {
         super(Properties.of().strength(1.0f).destroyTime(1.0f).sound(SoundType.CHAIN).mapColor(MapColor.METAL));
@@ -38,19 +33,8 @@ public class ReactorPortBlock extends BaseBlockBE {
     }
 
     @Override
-    protected MapCodec<? extends ReactorPortBlock> codec() {
-        return CODEC;
-    }
-
-    @Override
-    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
-            BlockHitResult hitResult) {
-
-        if (level.getBlockEntity(pos) instanceof ReactorPortBE be) {
-            player.openMenu(new SimpleMenuProvider(be, be.getContainerName()), pos);
-            return InteractionResult.SUCCESS;
-        }
-        return super.useWithoutItem(state, level, pos, player, hitResult);
+    protected Function<Properties, Block> getFactory() {
+        return ReactorPortBlock::new;
     }
 
     @Override
@@ -60,23 +44,13 @@ public class ReactorPortBlock extends BaseBlockBE {
     }
 
     @Override
-    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
-
-        if (state.getBlock() != newState.getBlock())
-            if (level.getBlockEntity(pos) instanceof ReactorPortBE be) {
-
-                be.drops();
-
-                level.updateNeighbourForOutputSignal(pos, this);
-            }
-
-        super.onRemove(state, level, pos, newState, movedByPiston);
-    }
-
-    @Override
     public void appendHoverText(ItemStack i, TooltipContext c, List<Component> t,
             TooltipFlag f) {
         t.add(Component.translatable(Main.ID + "." + zStatic.ReactorStuff.port));
+    }
+
+    @Override
+    protected void onClickAction(BlockState state, Level level, BlockPos pos, Player player) {
     }
 
 }
