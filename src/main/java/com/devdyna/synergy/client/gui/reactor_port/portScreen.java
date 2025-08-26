175d02080fe2ae92a8181daf4927ffef9513336f
DevDyna <andrearisorti2003@gmail.com>
2025-08-26 22:21:02 +0200

    Update portScreen.java

--- src/main/java/com/devdyna/synergy/client/gui/reactor_port/portScreen.java
+++ src/main/java/com/devdyna/synergy/client/gui/reactor_port/portScreen.java
@@ -1,21 +1,18 @@
 package com.devdyna.synergy.client.gui.reactor_port;
 
-import static com.devdyna.synergy.Main.ID;
+import java.util.Set;
 
+import com.devdyna.synergy.client.gui.screenLocations;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 @SuppressWarnings("null")
-public class portScreen extends AbstractContainerScreen<portGUI> {
-
-    private static final ResourceLocation GUI_TEXTURE = ResourceLocation.fromNamespaceAndPath(ID,
-            "textures/gui/single.png");
+public class portScreen extends AbstractContainerScreen<portGUI> implements screenLocations {
 
     public portScreen(portGUI menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -25,10 +22,15 @@ public class portScreen extends AbstractContainerScreen<portGUI> {
     protected void renderBg(GuiGraphics guiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, GUI_TEXTURE);
+
+        var textures = Set.of(PLAYER_INVENTORY, MACHINE_SINGLE_SLOT);
+
+        textures.forEach(t -> RenderSystem.setShaderTexture(0, t));
+
         int x = (width - imageWidth) / 2;
         int y = (height - imageHeight) / 2;
-        guiGraphics.blit(GUI_TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+
+        textures.forEach(t -> guiGraphics.blit(t, x, y, 0, 0, imageWidth, imageHeight));
     }
 
     @Override
