d9f680b8cffe7337452af3526332bfe175046029
DevDyna <andrearisorti2003@gmail.com>
2025-08-30 16:20:18 +0200

    Update SprinklerBE.java

--- src/main/java/com/devdyna/synergy/init/builder/sprinkler/SprinklerBE.java
+++ src/main/java/com/devdyna/synergy/init/builder/sprinkler/SprinklerBE.java
@@ -19,11 +19,11 @@ import net.neoforged.neoforge.energy.EnergyStorage;
 @SuppressWarnings("null")
 public class SprinklerBE extends BaseBE implements EnergyBlock {
 
-    private static int radius = 4;
-
     public SprinklerBE(BlockPos pos, BlockState state) {
         super(zBlockEntities.SPRINKLER.get(), pos, state);
     }
+
+    private static int radius = 4;
 
     @Override
     public void tickServer() {
@@ -32,10 +32,10 @@ public class SprinklerBE extends BaseBE implements EnergyBlock {
         var y = getBlockPos().getY();
         var z = getBlockPos().getZ();
 
-        level.setBlockAndUpdate(getBlockPos(), getBlockState().setValue(BlockStateProperties.ENABLED, canExtract() && !level.hasNeighborSignal(getBlockPos())));
+        level.setBlockAndUpdate(getBlockPos(), getBlockState().setValue(BlockStateProperties.ENABLED,
+                canExtract() && !level.hasNeighborSignal(getBlockPos())));
 
         if (getBlockState().getValue(BlockStateProperties.ENABLED)) {
-            
 
             BlockPos.randomBetweenClosed(level.random, 1,
                     x - radius, y, z - radius,
@@ -44,7 +44,7 @@ public class SprinklerBE extends BaseBE implements EnergyBlock {
                         BlockState state = level.getBlockState(pos);
                         if (state.isRandomlyTicking() && LevelUtil.chance(75, level)) {
                             if (LevelUtil.chance(25, level))
-                                LevelUtil.addParticle(ParticleTypes.HAPPY_VILLAGER,(ServerLevel) level, pos,  true);
+                                LevelUtil.addParticle(ParticleTypes.HAPPY_VILLAGER, (ServerLevel) level, pos, true);
 
                             if (LevelUtil.chance(25, level))
                                 level.playSound(null, getBlockPos(), SoundEvents.BONE_MEAL_USE, SoundSource.BLOCKS,
@@ -71,6 +71,11 @@ public class SprinklerBE extends BaseBE implements EnergyBlock {
     @Override
     public EnergyStorage getCapEnergy() {
         return getData(zHandlers.ENERGY_STORAGE);
+    }
+
+    @Override
+    public int MaxFE() {
+        return 10000;
     }
 
 }
