e7c6f0e88100b8a8c2f421cb4a919ada3ba1e2bf
DevDyna <andrearisorti2003@gmail.com>
2025-08-31 21:40:01 +0200

    Create TinyChestBE.java

---
+++ src/main/java/com/devdyna/synergy/init/builder/chests/TinyChestBE.java
@@ -0,0 +1,33 @@
+package com.devdyna.synergy.init.builder.chests;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.synergy.api.menu.BEMenu;
+import com.devdyna.synergy.client.gui.tiny_chest.chestGUI;
+import com.devdyna.synergy.init.types.zBlockEntities;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("null")
+public class TinyChestBE extends BEMenu {
+
+    public TinyChestBE(BlockPos pos, BlockState blockState) {
+        super(zBlockEntities.WOODEN_TINY_CHEST.get(), pos, blockState);
+    }
+
+    @Override
+    @Nullable
+    public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player) {
+        return new chestGUI(i, inventory, this);
+    }
+
+    @Override
+    public int MachineSlots() {
+        return 1;
+    }
+
+}
