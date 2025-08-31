394572da68b65135a6c78f2e1c65772f684f9133
DevDyna <andrearisorti2003@gmail.com>
2025-08-31 21:35:34 +0200

    reverted new->old BE

--- src/main/java/com/devdyna/synergy/init/types/zBlockEntities.java
+++ src/main/java/com/devdyna/synergy/init/types/zBlockEntities.java
@@ -2,11 +2,11 @@ package com.devdyna.synergy.init.types;
 
 import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.Main;
+import com.devdyna.synergy.init.Material;
 import com.devdyna.synergy.init.builder.chests.wchestTinyBE;
 import com.devdyna.synergy.init.builder.harvester.HarvesterBE;
 import com.devdyna.synergy.init.builder.pipeBlocks.nodes.blockentities.*;
 import com.devdyna.synergy.init.builder.reactor.controller.ReactorControllerBE;
-import com.devdyna.synergy.init.builder.reactor.controller.ReactorControllerBlock;
 import com.devdyna.synergy.init.builder.reactor.port.ReactorPortBE;
 import com.devdyna.synergy.init.builder.solar_panel.SolarPanelBE;
 import com.devdyna.synergy.init.builder.sprinkler.SprinklerBE;
@@ -16,7 +16,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-@SuppressWarnings("null")
+@SuppressWarnings({ "unchecked", "null" })
 public class zBlockEntities {
         public static void register(IEventBus bus) {
                 zBE.register(bus);
@@ -27,58 +27,36 @@ public class zBlockEntities {
                         .create(BuiltInRegistries.BLOCK_ENTITY_TYPE, Main.ID);
         // ---------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<SprinklerBE>> SPRINKLER = zBE
-                        .register(
-                                        zStatic.Blocks.sprinkler,
-                                        () -> BlockEntityType.Builder.of(SprinklerBE::new,
-                                                        zBlocks.SPRINKLER.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<SprinklerBE>> SPRINKLER = Material
+                        .createBlockEntity(zStatic.Blocks.sprinkler, SprinklerBE::new, zBlocks.SPRINKLER);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<HarvesterBE>> HARVESTER = zBE
-                        .register(
-                                        zStatic.Blocks.harvester,
-                                        () -> BlockEntityType.Builder.of(HarvesterBE::new,
-                                                        zBlocks.HARVESTER.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<HarvesterBE>> HARVESTER = Material
+                        .createBlockEntity(zStatic.Blocks.harvester, HarvesterBE::new, zBlocks.HARVESTER);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<SolarPanelBE>> SOLAR_PANEL = zBE
-                        .register(
-                                        zStatic.Blocks.solar_panel,
-                                        () -> BlockEntityType.Builder.of(SolarPanelBE::new,
-                                                        zBlocks.SOLAR_PANEL.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<SolarPanelBE>> SOLAR_PANEL = Material
+                        .createBlockEntity(zStatic.Blocks.solar_panel, SolarPanelBE::new, zBlocks.SOLAR_PANEL);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemTransferBE>> ITEM_TRANSFER = zBE
-                        .register(
-                                        zStatic.PipeStuff.nodes.ItemTransfer,
-                                        () -> BlockEntityType.Builder.of(ItemTransferBE::new,
-                                                        zBlocks.ITEM_TRANSFER.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemTransferBE>> ITEM_TRANSFER = Material
+                        .createBlockEntity(zStatic.PipeStuff.nodes.ItemTransfer, ItemTransferBE::new,
+                                        zBlocks.ITEM_TRANSFER);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemProviderBE>> ITEM_PROVIDER = zBE
-                        .register(
-                                        zStatic.PipeStuff.nodes.ItemProvider,
-                                        () -> BlockEntityType.Builder.of(ItemProviderBE::new,
-                                                        zBlocks.ITEM_PROVIDER.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemProviderBE>> ITEM_PROVIDER = Material
+                        .createBlockEntity(zStatic.PipeStuff.nodes.ItemProvider, ItemProviderBE::new,
+                                        zBlocks.ITEM_PROVIDER);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemRetrievalBE>> ITEM_RETRIEVAL = zBE
-                        .register(
-                                        zStatic.PipeStuff.nodes.ItemRetrieval,
-                                        () -> BlockEntityType.Builder.of(ItemRetrievalBE::new,
-                                                        zBlocks.ITEM_RETRIEVAL.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ItemRetrievalBE>> ITEM_RETRIEVAL = Material
+                        .createBlockEntity(zStatic.PipeStuff.nodes.ItemRetrieval, ItemRetrievalBE::new,
+                                        zBlocks.ITEM_RETRIEVAL);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReactorPortBE>> REACTOR_PORT = zBE
-                        .register(
-                                        zStatic.ReactorStuff.port,
-                                        () -> BlockEntityType.Builder.of(ReactorPortBE::new,
-                                                        zBlocks.REACTOR_PORT.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReactorControllerBE>> REACTOR_CONTROLLER = Material
+                        .createBlockEntity(zStatic.ReactorStuff.controller, ReactorControllerBE::new,
+                                        zBlocks.REACTOR_CONTROLLER);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReactorControllerBE>> REACTOR_CONTROLLER = zBE
-                        .register(
-                                        zStatic.ReactorStuff.controller,
-                                        () -> BlockEntityType.Builder.of(ReactorControllerBE::new,
-                                                        zBlocks.REACTOR_CONTROLLER.get()).build(null));
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<wchestTinyBE>> WOODEN_TINY_CHEST = Material
+                        .createBlockEntity(zStatic.Blocks.tiny_wooden_chest, wchestTinyBE::new,
+                                        zBlocks.WOODEN_TINY_CHEST);
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<wchestTinyBE>> WOODEN_TINY_CHEST = zBE
-                        .register(
-                                        zStatic.Blocks.tiny_wooden_chest,
-                                        () -> BlockEntityType.Builder.of(wchestTinyBE::new,
-                                                        zBlocks.WOODEN_TINY_CHEST.get()).build(null));
-
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReactorPortBE>> REACTOR_PORT = Material
+                        .createBlockEntity(zStatic.ReactorStuff.port, ReactorPortBE::new,
+                                        zBlocks.REACTOR_PORT);
 }
