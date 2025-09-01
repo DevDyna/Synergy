615d7fd83cd63c9fe61838eb18a22a4277f72165
DevDyna <andrearisorti2003@gmail.com>
2025-09-02 01:57:09 +0200

    Update BaseBlockBE.java

--- src/main/java/com/devdyna/synergy/api/coreBE/BaseBlockBE.java
+++ src/main/java/com/devdyna/synergy/api/coreBE/BaseBlockBE.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
+@SuppressWarnings("null")
 public abstract class BaseBlockBE extends Block implements EntityBlock {
 
     public BaseBlockBE(Properties properties) {
