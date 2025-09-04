457ab294059e577d6f6f61fec26157dd8d0a0565
DevDyna <andrearisorti2003@gmail.com>
2025-09-04 13:42:36 +0200

    Update zItemTag.java

--- src/main/java/com/devdyna/synergy/init/types/zItemTag.java
+++ src/main/java/com/devdyna/synergy/init/types/zItemTag.java
@@ -1,5 +1,6 @@
 package com.devdyna.synergy.init.types;
 
+import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.init.Material;
 
 import net.minecraft.tags.TagKey;
@@ -13,8 +14,12 @@ public class zItemTag {
 
     public static final TagKey<Item> STONE_SLABS = Material.tagItem("stone_slabs");
 
-    // public static final TagKey<Item> SMASHER = Material.tagItem("smasher");
-
     public static final TagKey<Item> AZALEA_BUSHES = Material.tagItem("azalea");
 
+    public static final TagKey<Item> CROP_BLUE_CUP = Material.tagItem("crops/"+zStatic.Plants.TYPE_BLUE_CUP,"c");
+    public static final TagKey<Item> CROP_VIOLET_WEBCAP = Material.tagItem("crops/"+zStatic.Plants.TYPE_VIOLET_WEBCAP,"c");
+    public static final TagKey<Item> CROP_RICE = Material.tagItem("crops/"+zStatic.Plants.RICE,"c");
+    public static final TagKey<Item> CROP_COTTON = Material.tagItem("crops/"+zStatic.Plants.COTTON,"c");
+    public static final TagKey<Item> CROP_CAVE_WHEAT = Material.tagItem("crops/"+zStatic.Plants.CAVE_WHEAT,"c");
+    public static final TagKey<Item> CROP_AZALEA = Material.tagItem("crops/potted_"+zStatic.Blocks.azalea,"c");
 }
