0b6680752c9f57b42bad5abf7c8388b723473a5e
DevDyna <andrearisorti2003@gmail.com>
2025-09-04 13:42:28 +0200

    Update DataMaps.java

--- src/main/java/com/devdyna/synergy/datagen/server/DataMaps.java
+++ src/main/java/com/devdyna/synergy/datagen/server/DataMaps.java
@@ -3,6 +3,8 @@ package com.devdyna.synergy.datagen.server;
 import java.util.concurrent.CompletableFuture;
 
 import com.devdyna.synergy.init.dataMaps.RecordMap.ProviderRecipes;
+import com.devdyna.synergy.init.types.zItems;
+
 import com.devdyna.synergy.init.dataMaps.zDataMaps;
 
 import net.minecraft.core.HolderLookup.Provider;
@@ -11,28 +13,55 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 
-@SuppressWarnings({ "deprecation" })
+@SuppressWarnings({ "deprecation", "null" })
 public class DataMaps extends DataMapProvider {
 
-    public DataMaps(PackOutput o, CompletableFuture<Provider> p) {
-        super(o, p);
-    }
+        public DataMaps(PackOutput o, CompletableFuture<Provider> p) {
+                super(o, p);
+        }
 
-    @Override
-    protected void gather(Provider p) {
+        @Override
+        protected void gather(Provider p) {
 
-        builder(zDataMaps.PROVIDER_RECIPES).add(Items.COBBLESTONE.builtInRegistryHolder(),
-                new ProviderRecipes(Blocks.AIR.defaultBlockState(),
-                        new BlockState[] { Blocks.LAVA.defaultBlockState(), Blocks.WATER.defaultBlockState() }),
-                false);
+                builder(zDataMaps.PROVIDER_RECIPES).add(Items.COBBLESTONE.builtInRegistryHolder(),
+                                new ProviderRecipes(Blocks.AIR.defaultBlockState(),
+                                                new BlockState[] { Blocks.LAVA.defaultBlockState(),
+                                                                Blocks.WATER.defaultBlockState() }),
+                                false);
 
-        builder(zDataMaps.PROVIDER_RECIPES).add(Items.BASALT.builtInRegistryHolder(),
-                new ProviderRecipes(Blocks.SOUL_SOIL.defaultBlockState(),
-                        new BlockState[] { Blocks.LAVA.defaultBlockState(), Blocks.BLUE_ICE.defaultBlockState() }),
-                false);
+                builder(zDataMaps.PROVIDER_RECIPES).add(Items.BASALT.builtInRegistryHolder(),
+                                new ProviderRecipes(Blocks.SOUL_SOIL.defaultBlockState(),
+                                                new BlockState[] { Blocks.LAVA.defaultBlockState(),
+                                                                Blocks.BLUE_ICE.defaultBlockState() }),
+                                false);
 
+                builder(NeoForgeDataMaps.COMPOSTABLES)
+                                .add(zItems.COTTON_SEEDS.get().builtInRegistryHolder(), new Compostable(0.1F), false)
+                                .add(zItems.COTTON.get().builtInRegistryHolder(), new Compostable(0.25F), false)
 
-    }
+                                .add(zItems.BLUE_CUP_MUSHROOM.get().builtInRegistryHolder(), new Compostable(0.25F),
+                                                false)
+                                .add(zItems.BLUE_CUP_SPORE.get().builtInRegistryHolder(), new Compostable(0.15F), false)
+
+                                .add(zItems.VIOLET_WEBCAP_MUSHROOM.get().builtInRegistryHolder(),
+                                                new Compostable(0.25F), false)
+                                .add(zItems.VIOLET_WEBCAP_SPORE.get().builtInRegistryHolder(), new Compostable(0.155F),
+                                                false)
+
+                                .add(zItems.RICE_SEED.get().builtInRegistryHolder(), new Compostable(0.2F), false)
+
+                                .add(zItems.CAVE_WHEAT_SEEDS.get().builtInRegistryHolder(), new Compostable(0.6F),
+                                                false)
+
+                                .add(zItems.AZALEA_SEEDS.get().builtInRegistryHolder(), new Compostable(0.1F), false)
+                                .add(zItems.SMALL_AZALEA_LEAF.get().builtInRegistryHolder(), new Compostable(0.105F),
+                                                false)
+                                .add(zItems.SMALL_AZALEA_ROOTS.get().builtInRegistryHolder(), new Compostable(0.15F),
+                                                false);
+
+        }
 
 }
