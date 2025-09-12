ec8b28a894464b08ec4917d2db1ef72b09ad91cb
DevDyna <andrearisorti2003@gmail.com>
2025-09-12 20:56:32 +0200

    Update x.java

--- src/main/java/com/devdyna/synergy/utils/x.java
+++ src/main/java/com/devdyna/synergy/utils/x.java
@@ -3,29 +3,39 @@ package com.devdyna.synergy.utils;
 import net.minecraft.core.DefaultedRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 import static com.devdyna.synergy.Main.ID;
 
 public class x {
 
-    public static ResourceLocation rl(String s) {
-        return ResourceLocation.fromNamespaceAndPath(ID, s);
-    }
-
     public static ResourceLocation rl(String modid, String s) {
         return ResourceLocation.fromNamespaceAndPath(modid, s);
     }
 
+    public static ResourceLocation rl(String s) {
+        return rl(ID, s);
+    }
+
     /**
-     * @param <T>
-     * @param d   <code>BuiltInRegistries.BLOCK</code>
-     * @param i   <code>Blocks.STONE</code>
+     * @param d <code>BuiltInRegistries.BLOCK</code>
+     * @param i <code>Blocks.STONE</code>
      */
     public static <T> ResourceLocation rl(DefaultedRegistry<T> d, T i) {
-        return ResourceLocation.fromNamespaceAndPath(ID, d.getKey(i).getPath());
+        return rl(d.getKey(i).getPath());
+    }
+
+    /**
+     * @param d <code>BuiltInRegistries.BLOCK</code>
+     * @param i <code>Blocks.STONE</code>
+     */
+    public static <T> ResourceLocation rl(DefaultedRegistry<T> d, T i, String modid) {
+        return rl(modid, d.getKey(i).getPath());
     }
 
     public static ResourceLocation rl(Item i) {
@@ -37,9 +47,8 @@ public class x {
     }
 
     /**
-     * @param <T>
-     * @param d   <code>BuiltInRegistries.BLOCK</code>
-     * @param i   <code>Blocks.STONE</code>
+     * @param d <code>BuiltInRegistries.BLOCK</code>
+     * @param i <code>Blocks.STONE</code>
      */
     public static <T> String path(DefaultedRegistry<T> d, T i) {
         return d.getKey(i).getPath();
@@ -73,5 +82,21 @@ public class x {
 
     public static ItemStack item(Item i, int c) {
         return new ItemStack(i, c);
+    }
+
+    public static ItemStack item(DeferredHolder<Item, Item> i, int c) {
+        return item(i.get(), c);
+    }
+
+    public static Ingredient ingredient(Item i) {
+        return Ingredient.of(i);
+    }
+
+    public static Ingredient ingredient(DeferredHolder<Item, Item> i) {
+        return ingredient(i.get());
+    }
+
+    public static Ingredient ingredient(TagKey<Item> i) {
+        return Ingredient.of(i);
     }
 }
