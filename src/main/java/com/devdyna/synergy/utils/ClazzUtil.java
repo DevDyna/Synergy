8dc2ebc965cf4c82a4dd030dbebe237b0410eea9
DevDyna <andrearisorti2003@gmail.com>
2025-09-13 22:57:42 +0200

    Update ClazzUtil.java

--- src/main/java/com/devdyna/synergy/utils/ClazzUtil.java
+++ src/main/java/com/devdyna/synergy/utils/ClazzUtil.java
@@ -7,21 +7,24 @@ import java.util.List;
 import com.devdyna.synergy.init.types.zItems;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
+@SuppressWarnings("unchecked")
 public class ClazzUtil {
+
+    /**
+     * Dont use on LootTableProviders
+     */
     public static List<DeferredHolder<Item, ?>> getAllzItems() {
         List<DeferredHolder<Item, ?>> items = new ArrayList<>();
 
         for (Field field : zItems.class.getDeclaredFields()) {
             try {
-                // Only pick DeferredHolder<Item, ?>
                 if (DeferredHolder.class.isAssignableFrom(field.getType())) {
-                    Object value = field.get(null); // static field → null instance
+                    Object value = field.get(null);
                     if (value instanceof DeferredHolder<?, ?> holder) {
-                        // Ensure it's an Item DeferredHolder
                         if (holder.value() instanceof Item) {
-                            @SuppressWarnings("unchecked")
                             DeferredHolder<Item, ?> itemHolder = (DeferredHolder<Item, ?>) holder;
                             items.add(itemHolder);
                         }
@@ -33,4 +36,29 @@ public class ClazzUtil {
         }
         return items;
     }
+
+    /**
+     * Dont use on LootTableProviders
+     */
+    public static List<DeferredHolder<Block, ?>> getAllzBlocks() {
+        List<DeferredHolder<Block, ?>> blocks = new ArrayList<>();
+
+        for (Field field : zItems.class.getDeclaredFields()) {
+            try {
+                if (DeferredHolder.class.isAssignableFrom(field.getType())) {
+                    Object value = field.get(null);
+                    if (value instanceof DeferredHolder<?, ?> holder) {
+                        if (holder.value() instanceof Block) {
+                            DeferredHolder<Block, ?> blockHolder = (DeferredHolder<Block, ?>) holder;
+                            blocks.add(blockHolder);
+                        }
+                    }
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return blocks;
+    }
+
 }
