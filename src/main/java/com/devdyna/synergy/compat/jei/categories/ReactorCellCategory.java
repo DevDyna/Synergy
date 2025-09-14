26f8779a791620b6d6e5651c73bc2a0b7feeddc8
DevDyna <andrearisorti2003@gmail.com>
2025-09-15 01:42:17 +0200

    Update ReactorCellCategory.java

--- src/main/java/com/devdyna/synergy/compat/jei/categories/ReactorCellCategory.java
+++ src/main/java/com/devdyna/synergy/compat/jei/categories/ReactorCellCategory.java
@@ -7,12 +7,12 @@ import org.jetbrains.annotations.Nullable;
 import com.devdyna.synergy.Main;
 import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.client.gui.screenLocations;
+import com.devdyna.synergy.compat.jei.drawable.SimpleIcon;
 import com.devdyna.synergy.init.recipeTypes.type.FuelCellRecipe;
 import com.devdyna.synergy.init.types.zItems;
 import com.devdyna.synergy.init.types.zRecipeTypes;
 import com.devdyna.synergy.utils.x;
 
-import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
@@ -24,7 +24,6 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
 
 @SuppressWarnings("null")
 public class ReactorCellCategory implements IRecipeCategory<FuelCellRecipe> {
@@ -51,7 +50,7 @@ public class ReactorCellCategory implements IRecipeCategory<FuelCellRecipe> {
 
     @Override
     public @Nullable IDrawable getIcon() {
-        return helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(zItems.URANIUM));
+        return SimpleIcon.of(helper, zItems.URANIUM.get());
     }
 
     @Override
@@ -82,10 +81,10 @@ public class ReactorCellCategory implements IRecipeCategory<FuelCellRecipe> {
                                 : recipe.getDuration() + " tick" + (recipe.getDuration() > 1 ? "s" : ""))),
                 45, 5, 0xA0A0A0);
 
-        guiGraphics.drawString(font, (recipe.getHeat() >= 0 ? "Heat   +" : "") + recipe.getHeat() + "°/t",
+        guiGraphics.drawString(font, "Heat "+(recipe.getHeat() >= 0 ? "+" : "") + recipe.getHeat() + "°/t",
                 25, 40, 0xA0A0A0);
 
-        guiGraphics.drawString(font, (recipe.getFe() >= 0 ? "FEGen +" : "") + recipe.getFe() + " /t",
+        guiGraphics.drawString(font, "Gen  "+(recipe.getFe() >= 0 ? "+" : "") + recipe.getFe() + "fe/t",
                 25, 50, 0xA0A0A0);
 
     }
