a2b0b0c70f946974f3d53ee82c1d404b3b344ef3
DevDyna <andrearisorti2003@gmail.com>
2025-09-19 01:09:06 +0200

    Update RecordMap.java

--- src/main/java/com/devdyna/synergy/init/dataMaps/RecordMap.java
+++ src/main/java/com/devdyna/synergy/init/dataMaps/RecordMap.java
@@ -1,20 +1,13 @@
 package com.devdyna.synergy.init.dataMaps;
 
-import java.util.Arrays;
-
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-
-import net.minecraft.world.level.block.state.BlockState;
-
 public class RecordMap {
-    public record ProviderRecipes(BlockState belowBlock, BlockState... blocksToCheck) {
+    // public record CropResultMap(Ingredient crop,List<Ingredient> result_drop) {
 
-        public static final Codec<ProviderRecipes> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                Codec.list(BlockState.CODEC).fieldOf("blocksToCheck").forGetter(r -> Arrays.asList(r.blocksToCheck)),
-                BlockState.CODEC.fieldOf("belowBlock").forGetter(ProviderRecipes::belowBlock))
-                .apply(instance, (blocksList, belowBlock) -> new ProviderRecipes(belowBlock,
-                        blocksList.toArray(new BlockState[0]))));
-    }
+    // public static final Codec<CropResultMap> CODEC =
+    // RecordCodecBuilder.create(instance -> instance.group(
+    // Ingredient.CODEC.fieldOf("crop").forGetter(CropResultMap::crop),
+    // Ingredient.LIST_CODEC_NONEMPTY.fieldOf("drops").forGetter(CropResultMap::result_drop))
+    // .apply(instance, (c, r) -> new CropResultMap(c,r)));
+    // }
 
 }
