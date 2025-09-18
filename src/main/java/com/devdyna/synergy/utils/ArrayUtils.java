566975e7827d99239ceb691f92e0f073b83b693a
DevDyna <andrearisorti2003@gmail.com>
2025-09-19 01:36:04 +0200

    Update ArrayUtils.java

--- src/main/java/com/devdyna/synergy/utils/ArrayUtils.java
+++ src/main/java/com/devdyna/synergy/utils/ArrayUtils.java
@@ -1,13 +1,12 @@
 package com.devdyna.synergy.utils;
 
-import net.minecraft.world.item.Item;
+import java.util.*;
 
 public class ArrayUtils {
 
-    public static Item[] concatMixed(Item basic, Item... dynamic) {
-        Item[] combined = new Item[1 + dynamic.length];
-        combined[0] = basic;
-        System.arraycopy(dynamic, 0, combined, 1, dynamic.length);
-        return combined;
+    public static String[] concat(String[] arr1, String[] arr2) {
+        List<String> list = new ArrayList<>(Arrays.asList(arr1));
+        list.addAll(Arrays.asList(arr2));
+        return list.toArray(new String[0]);
     }
 }
