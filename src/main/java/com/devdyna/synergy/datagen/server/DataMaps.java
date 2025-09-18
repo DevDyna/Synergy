d08b53c0b43226a925a41de4d351f59e3687b0ac
DevDyna <andrearisorti2003@gmail.com>
2025-09-19 01:09:08 +0200

    Update DataMaps.java

--- src/main/java/com/devdyna/synergy/datagen/server/DataMaps.java
+++ src/main/java/com/devdyna/synergy/datagen/server/DataMaps.java
@@ -2,20 +2,11 @@ package com.devdyna.synergy.datagen.server;
 
 import java.util.concurrent.CompletableFuture;
 
-import com.devdyna.synergy.init.dataMaps.RecordMap.ProviderRecipes;
 import com.devdyna.synergy.init.types.zItems;
-
-import com.devdyna.synergy.init.dataMaps.zDataMaps;
-
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.common.data.DataMapProvider;
-import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
-import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
-import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+import net.neoforged.neoforge.registries.datamaps.builtin.*;
 
 @SuppressWarnings({ "deprecation", "null" })
 public class DataMaps extends DataMapProvider {
@@ -26,18 +17,6 @@ public class DataMaps extends DataMapProvider {
 
         @Override
         protected void gather(Provider p) {
-
-                builder(zDataMaps.PROVIDER_RECIPES).add(Items.COBBLESTONE.builtInRegistryHolder(),
-                                new ProviderRecipes(Blocks.AIR.defaultBlockState(),
-                                                new BlockState[] { Blocks.LAVA.defaultBlockState(),
-                                                                Blocks.WATER.defaultBlockState() }),
-                                false);
-
-                builder(zDataMaps.PROVIDER_RECIPES).add(Items.BASALT.builtInRegistryHolder(),
-                                new ProviderRecipes(Blocks.SOUL_SOIL.defaultBlockState(),
-                                                new BlockState[] { Blocks.LAVA.defaultBlockState(),
-                                                                Blocks.BLUE_ICE.defaultBlockState() }),
-                                false);
 
                 builder(NeoForgeDataMaps.COMPOSTABLES)
                                 .add(zItems.COTTON_SEEDS.get().builtInRegistryHolder(), new Compostable(0.1F), false)
@@ -63,8 +42,8 @@ public class DataMaps extends DataMapProvider {
                                 .add(zItems.SMALL_AZALEA_ROOTS.get().builtInRegistryHolder(), new Compostable(0.15F),
                                                 false);
 
-                        builder(NeoForgeDataMaps.FURNACE_FUELS)
-                        .add(zItems.INFERNAL_EMBER.get().builtInRegistryHolder(), new FurnaceFuel(3200), false);
+                builder(NeoForgeDataMaps.FURNACE_FUELS)
+                                .add(zItems.INFERNAL_EMBER.get().builtInRegistryHolder(), new FurnaceFuel(3200), false);
 
         }
 
