4e61dc74d9837ea90a360309ce70898c0c8263be
DevDyna <andrearisorti2003@gmail.com>
2025-09-18 20:13:21 +0200

    Create ItemToolTipped.java

---
+++ src/main/java/com/devdyna/synergy/init/builder/ItemToolTipped.java
@@ -0,0 +1,30 @@
+package com.devdyna.synergy.init.builder;
+
+import java.util.List;
+
+import com.devdyna.synergy.Main;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+@SuppressWarnings("null")
+public class ItemToolTipped extends Item {
+
+    private String traslationkey;
+    public ItemToolTipped(Properties properties,String traslationkey) {
+        super(properties);
+        this.traslationkey = traslationkey;
+    }
+
+    public ItemToolTipped(String traslationkey){
+        this(new Item.Properties(), traslationkey);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack i, TooltipContext c, List<Component> t,
+            TooltipFlag f) {
+        t.add(Component.translatable(Main.ID +"."+ traslationkey));
+    }
+
+}
