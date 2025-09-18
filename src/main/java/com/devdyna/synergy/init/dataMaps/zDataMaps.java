842682a8976fc1195c1080dd9915166ce4f590ce
DevDyna <andrearisorti2003@gmail.com>
2025-09-19 01:09:04 +0200

    Update zDataMaps.java

--- src/main/java/com/devdyna/synergy/init/dataMaps/zDataMaps.java
+++ src/main/java/com/devdyna/synergy/init/dataMaps/zDataMaps.java
@@ -1,24 +1,18 @@
 package com.devdyna.synergy.init.dataMaps;
 
-import static com.devdyna.synergy.Main.ID;
-
-import com.devdyna.synergy.init.dataMaps.RecordMap.ProviderRecipes;
-
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.registries.datamaps.DataMapType;
 import net.neoforged.neoforge.registries.datamaps.RegisterDataMapTypesEvent;
 
 public class zDataMaps {
-            @SubscribeEvent
+        @SubscribeEvent
         public static void register(RegisterDataMapTypesEvent event) {
-                event.register(PROVIDER_RECIPES);
+                // event.register(CROP_RESULT);
         }
 
-        public static final DataMapType<Item, ProviderRecipes> PROVIDER_RECIPES = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "provider/recipes"),
-                        Registries.ITEM,
-                        ProviderRecipes.CODEC).synced(ProviderRecipes.CODEC, false).build();
+        // public static final DataMapType<Item, CropResultMap> CROP_RESULT =
+        // DataMapType.builder(
+        // x.rl(ID, "crop_result"),
+        // Integer.TYPE,
+        // CropResultMap.CODEC).synced(CropResultMap.CODEC, false).build();
+
 }
