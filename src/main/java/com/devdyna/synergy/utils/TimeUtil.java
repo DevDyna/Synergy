9207bc10f67f6353183d2f1c326a4223a1dd59ce
DevDyna <andrearisorti2003@gmail.com>
2025-09-19 15:49:58 +0200

    Create TimeUtil.java

---
+++ src/main/java/com/devdyna/synergy/utils/TimeUtil.java
@@ -0,0 +1,12 @@
+package com.devdyna.synergy.utils;
+
+public class TimeUtil {
+
+    public static long ONE_SECOND = 1000;
+
+    public static boolean fireAt(long tick) {
+        long intervalStart = tick / 2;
+        return (System.currentTimeMillis() % tick) < intervalStart;
+    }
+
+}
