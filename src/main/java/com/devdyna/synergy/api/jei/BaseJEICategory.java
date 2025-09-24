2cb96ca2cd58447012be6c17f0d1db43fb569a48
DevDyna <andrearisorti2003@gmail.com>
2025-09-24 23:21:52 +0200

    Create BaseJEICategory.java

---
+++ src/main/java/com/devdyna/synergy/api/jei/BaseJEICategory.java
@@ -0,0 +1,9 @@
+package com.devdyna.synergy.api.jei;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+
+public class BaseJEICategory {
+    protected final Font font = Minecraft.getInstance().font;
+
+}
