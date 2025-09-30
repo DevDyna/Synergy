1cceec787b36ba31d1f1495b8894f3cbcf1cb9cd
DevDyna <andrearisorti2003@gmail.com>
2025-09-30 14:44:56 +0200

    Update core.java

--- src/main/java/com/devdyna/synergy/compat/core.java
+++ src/main/java/com/devdyna/synergy/compat/core.java
@@ -4,6 +4,7 @@ import static com.devdyna.synergy.Main.ID;
 
 import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.utils.LogUtil;
+import com.devdyna.synergy.utils.ModAddonUtil;
 import com.devdyna.synergy.utils.StringUtil;
 
 import guideme.Guide;
@@ -13,18 +14,20 @@ public class core {
 
     public static void registerCompat() {
         LogUtil.decor(20);
-        LogUtil.info(StringUtil.nameCapitalized(ID)+" Compatibility Checker started");
+        LogUtil.info(StringUtil.nameCapitalized(ID) + " Compatibility Checker started");
         LogUtil.decor(20);
-        core.createGuide();
-        LogUtil.decor(20);
-    }
-
-    private static void createGuide() {
 
         LogUtil.info("GuideMe"
-                + (zStatic.checkMods.GuideMe ? " found" : " not found"));
-        if (zStatic.checkMods.GuideMe)
+                + (!ModAddonUtil.checkMod(zStatic.Mods.GuideMe) ? " " : " not") + " found");
+        if (!ModAddonUtil.checkMod(zStatic.Mods.GuideMe))
             Guide.builder(ResourceLocation.parse(ID + ":guide")).build();
 
+        LogUtil.info("FarmersDelight"
+                + (!ModAddonUtil.checkMod(zStatic.Mods.FarmersDelight) ? " " : " not") + " found");
+
+        LogUtil.info("Mekanism"
+                + (!ModAddonUtil.checkMod(zStatic.Mods.Mekanism) ? " " : " not") + " found");
+
+        LogUtil.decor(20);
     }
 }
