727bc0aacc838501ac19ad11ce11b636d2c646ce
DevDyna <andrearisorti2003@gmail.com>
2025-10-03 01:15:52 +0200

    Update zMultiTags.java

--- src/main/java/com/devdyna/synergy/init/types/zMultiTags.java
+++ src/main/java/com/devdyna/synergy/init/types/zMultiTags.java
@@ -1,8 +1,5 @@
 package com.devdyna.synergy.init.types;
 
-import com.devdyna.synergy.zStatic;
-import com.devdyna.synergy.api.MultiTag;
-
 @Deprecated
 public class zMultiTags {
 
@@ -79,17 +76,5 @@ public class zMultiTags {
     // public static MultiTag deposit(String name) {
     // return new MultiTag(ID, "deposit/", name);
     // }
-
-    // public static MultiTag COOLERS = MultiTag.of("cooler");
-    // public static MultiTag MODERATORS = MultiTag.of("moderator");
-
-    // public static MultiTag CAN_SUSTAIN_RICE = MultiTag.of("can_sustain/" + zStatic.Plants.RICE);
-    // public static MultiTag CAN_SUSTAIN_COTTON = MultiTag.of("can_sustain/" + zStatic.Plants.COTTON);
-    // public static MultiTag CAN_SUSTAIN_CAVE_WHEAT = MultiTag.of("can_sustain/" + zStatic.Plants.CAVE_WHEAT);
-    // public static MultiTag CAN_SUSTAIN_BLUE_CUP = MultiTag.of("can_sustain/" + zStatic.Plants.BLUE_CUP_MUSHROOM);
-    // public static MultiTag CAN_SUSTAIN_VIOLET_WEBCAP = MultiTag
-    //         .of("can_sustain/" + zStatic.Plants.VIOLET_WEBCAP_MUSHROOM);
-
-    // public static MultiTag INFESTED_BLOCKS = MultiTag.of("c", "infested_blocks");
 
 }
