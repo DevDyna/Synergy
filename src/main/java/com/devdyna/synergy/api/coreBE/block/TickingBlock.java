0bfe28effd6990ec7756ce8a178fdb7b64473ed5
DevDyna <andrearisorti2003@gmail.com>
2025-10-20 12:11:19 +0200

    Create TickingBlock.java

---
+++ src/main/java/com/devdyna/synergy/api/coreBE/block/TickingBlock.java
@@ -0,0 +1,37 @@
+package com.devdyna.synergy.api.coreBE.block;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.synergy.api.coreBE.be.*;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("null")
+public abstract class TickingBlock extends Block implements EntityBlock {
+
+    public TickingBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level l, BlockState s,
+            BlockEntityType<T> ty) {
+        return (lvl, pos, b, t) -> {
+            if (t instanceof TickingBE be) {
+                be.tickBoth();
+                if (l.isClientSide())
+                    be.tickClient();
+                else
+                    be.tickServer();
+            }
+        };
+    }
+
+}
