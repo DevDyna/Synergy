14abfbcaa42b9cb0a950f4c99e34f010322f0e37
DevDyna <andrearisorti2003@gmail.com>
2025-10-20 12:11:48 +0200

    Create BEMenu.java

---
+++ src/main/java/com/devdyna/synergy/api/coreBE/be/BEMenu.java
@@ -0,0 +1,39 @@
+package com.devdyna.synergy.api.coreBE.be;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.synergy.api.beLogic.MenuProvider;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("null")
+public abstract class BEMenu extends BlockEntity implements MenuProvider {
+
+    public BEMenu(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
+        super(type, pos, blockState);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+        return saveWithoutMetadata(pRegistries);
+    }
+
+    @Override
+    public Component getContainerName() {
+        return Component.translatable(this.getBlockState().getBlock().getDescriptionId());
+    }
+}
