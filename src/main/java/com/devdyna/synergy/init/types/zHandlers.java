3b236ca1184211ad0c59f938fce214e728525331
DevDyna <andrearisorti2003@gmail.com>
2025-10-21 21:22:34 +0200

    Update zHandlers.java

--- src/main/java/com/devdyna/synergy/init/types/zHandlers.java
+++ src/main/java/com/devdyna/synergy/init/types/zHandlers.java
@@ -28,14 +28,14 @@ public class zHandlers {
 
     public static final Supplier<AttachmentType<EnergyStorage>> ENERGY_STORAGE = zHandler.register(
             "energy_storage",
-            () -> AttachmentType.serializable(b -> (b instanceof EnergyBlock be)
+            () -> AttachmentType.serializable(h -> (h instanceof EnergyBlock be)
                     ? new EnergyStorage(be.MaxFE())
                     : null).build());
 
     public static final Supplier<AttachmentType<ItemStackHandler>> ITEM_STORAGE = zHandler.register(
-            "item_storage", () -> AttachmentType.serializable(holder -> {
-                if (holder instanceof ItemStorageBlock baseMachineBE)
-                    return new ItemStackHandler(baseMachineBE.MachineSlots());
+            "item_storage", () -> AttachmentType.serializable(h -> {
+                if (h instanceof ItemStorageBlock be)
+                    return new ItemStackHandler(be.MachineSlots());
                 return new ItemStackHandler(1);
             }).build());
 
