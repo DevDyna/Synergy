dece7820d23ec580e78cd7cf0c9eddd055483eea
DevDyna <andrearisorti2003@gmail.com>
2025-10-23 01:18:28 +0200

    Delete BaseJEICategory.java

--- src/main/java/com/devdyna/synergy/api/jei/BaseJEICategory.java
+++
@@ -1,9 +0,0 @@
-package com.devdyna.synergy.api.jei;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-
-public class BaseJEICategory {
-    protected final Font font = Minecraft.getInstance().font;
-
-}
