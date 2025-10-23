f4621af64617300be6b6d9ec3f374c2ac0f6048b
DevDyna <andrearisorti2003@gmail.com>
2025-10-23 21:15:45 +0200

    Update GameEvents.java

--- src/main/java/com/devdyna/synergy/GameEvents.java
+++ src/main/java/com/devdyna/synergy/GameEvents.java
@@ -9,7 +9,7 @@ public class GameEvents {
     public static void register() {
 
         NeoForge.EVENT_BUS.register(CrookBreakBlocks.class);
-        NeoForge.EVENT_BUS.register(BlockInjection.class);
+        // NeoForge.EVENT_BUS.register(BlockInjection.class);
         NeoForge.EVENT_BUS.register(ItemUseRecipeEvent.class);
         NeoForge.EVENT_BUS.register(ClayBrickPlacement.class);
         NeoForge.EVENT_BUS.register(ItemTooltipEvents.class);
