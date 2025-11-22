e1613ec85ccfee39a2e4db14c0c7123c26e87a47
DevDyna <andrearisorti2003@gmail.com>
2025-11-23 00:14:45 +0100

    added new api

---
+++ src/main/java/com/devdyna/synergy/utils/Image.java
@@ -0,0 +1,80 @@
+package com.devdyna.synergy.utils;
+
+import static com.devdyna.synergy.Main.ID;
+
+import mezz.jei.api.helpers.IGuiHelper;
+import net.minecraft.client.gui.GuiGraphics;
+
+public class Image {
+
+    private int x;
+    private int y;
+    private String rl;
+    private String modid = ID;
+    private int xo = 0;
+    private int yo = 0;
+
+    private int u = 0;
+    private int v = 0;
+
+    public Image() {
+
+    }
+
+    public static Image of() {
+        return new Image();
+    }
+
+    public Image size(int x, int y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Image uv(int u, int v) {
+        this.u = u;
+        this.v = v;
+        return this;
+    }
+
+    public Image offset(int xo, int yo) {
+        this.xo = xo;
+        this.yo = yo;
+        return this;
+    }
+
+    public Image rl(String image) {
+        this.rl = image;
+        return this;
+    }
+
+    public Image rl(String modid, String image) {
+        this.modid = modid;
+        this.rl = image;
+        return this;
+    }
+
+    /**
+     * JEI usage
+     */
+    public void render(IGuiHelper h, GuiGraphics g) {
+        h.drawableBuilder(com.devdyna.synergy.utils.x.rl(modid, rl), u, v, x, y).setTextureSize(x, y).build()
+                .draw(g, xo, yo);
+    }
+
+    /**
+     * not JEI usage!
+     */
+    public void render(GuiGraphics g) {
+
+        g.blit(
+                com.devdyna.synergy.utils.x.rl(modid, rl),
+                xo - 1,
+                yo - 1,
+                u, v,
+                x, y,
+                x, y);
+
+    }
+
+}
