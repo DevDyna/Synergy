c77477fb5ccc0d39316b274c563f87127989d8c1
DevDyna <andrearisorti2003@gmail.com>
2025-11-23 17:52:28 +0100

    color applicator item event tooltip

--- src/main/java/com/devdyna/synergy/events/ItemTooltipEvents.java
+++ src/main/java/com/devdyna/synergy/events/ItemTooltipEvents.java
@@ -8,14 +8,35 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 public class ItemTooltipEvents {
+    
     @SubscribeEvent
-    public static void itemTooltipEvent(ItemTooltipEvent event) {
+    public static void itemTooltipPlaceable(ItemTooltipEvent event) {
 
         var item = event.getItemStack();
         var tooltip = event.getToolTip();
 
         if (item.is(zItemTag.PLACEABLE)) {
             tooltip.add(Component.translatable(Main.ID + ".placed"));
+        }
+    }
+
+    @SubscribeEvent
+    public static void itemTooltipLaserColorApplicator(ItemTooltipEvent event) {
+
+        var item = event.getItemStack();
+        var tooltip = event.getToolTip();
+
+        if (item.is(zItemTag.DYE_RESET)) {
+            tooltip.add(Component.translatable(Main.ID + ".laser_use.reset"));
+        }
+        if (item.is(zItemTag.DYE_RED)) {
+            tooltip.add(Component.translatable(Main.ID + ".laser_use.red"));
+        }
+        if (item.is(zItemTag.DYE_GREEN)) {
+            tooltip.add(Component.translatable(Main.ID + ".laser_use.green"));
+        }
+        if (item.is(zItemTag.DYE_BLUE)) {
+            tooltip.add(Component.translatable(Main.ID + ".laser_use.blue"));
         }
     }
 }
