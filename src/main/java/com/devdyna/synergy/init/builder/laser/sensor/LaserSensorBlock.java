f0656660bc43b597cbb94ff5e225b05fd815ce05
DevDyna <andrearisorti2003@gmail.com>
2025-11-23 17:51:54 +0100

    Update LaserSensorBlock.java

--- src/main/java/com/devdyna/synergy/init/builder/laser/sensor/LaserSensorBlock.java
+++ src/main/java/com/devdyna/synergy/init/builder/laser/sensor/LaserSensorBlock.java
@@ -2,11 +2,15 @@ package com.devdyna.synergy.init.builder.laser.sensor;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.devdyna.synergy.Main;
 import com.devdyna.synergy.zStatic;
+import com.devdyna.synergy.api.coreBE.block.TickingBlock;
 import com.devdyna.synergy.init.builder.laser.IBlockLaser;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -14,10 +18,10 @@ import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -26,9 +30,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 
 @SuppressWarnings("null")
-public class LaserSensorBlock extends Block implements IBlockLaser {
-
-    public final static BooleanProperty ROTATED = BooleanProperty.create("rotated");
+public class LaserSensorBlock extends TickingBlock implements IBlockLaser {
 
     public LaserSensorBlock() {
         super(getProperties);
@@ -42,25 +44,36 @@ public class LaserSensorBlock extends Block implements IBlockLaser {
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext c) {
         return this.defaultBlockState()
-                .setValue(ROTATED, false)
+                .setValue(BlockStateProperties.INVERTED, List.of(Direction.WEST,Direction.EAST).contains(c.getHorizontalDirection()))
                 .setValue(BlockStateProperties.ENABLED, false);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> b) {
-        b.add(BlockStateProperties.ENABLED, ROTATED);
+        b.add(BlockStateProperties.ENABLED, BlockStateProperties.INVERTED);
+    }
+
+    @Override
+    @Nullable
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new LaserSensorBE(pos, state);
     }
 
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
             BlockHitResult hitResult) {
-        level.setBlockAndUpdate(pos, level.getBlockState(pos).cycle(ROTATED));
+        level.setBlockAndUpdate(pos, level.getBlockState(pos).cycle(BlockStateProperties.INVERTED));
         return InteractionResult.SUCCESS;
     }
 
     @Override
     protected boolean isSignalSource(BlockState state) {
-        return state.getValue(BlockStateProperties.ENABLED);
+        return true;
+    }
+
+    @Override
+    protected int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(BlockStateProperties.ENABLED) ? 15 : 0;
     }
 
     @Override
