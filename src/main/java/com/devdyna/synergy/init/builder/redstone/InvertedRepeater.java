70c60b885ca6dcbee29ab8a41bd09ac35ac0081b
DevDyna <andrearisorti2003@gmail.com>
2025-11-30 22:48:06 +0100

    Create InvertedRepeater.java

---
+++ src/main/java/com/devdyna/synergy/init/builder/redstone/InvertedRepeater.java
@@ -0,0 +1,81 @@
+package com.devdyna.synergy.init.builder.redstone;
+
+import com.mojang.serialization.MapCodec;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DiodeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+@SuppressWarnings("null")
+public class InvertedRepeater extends DiodeBlock {
+
+    public static final IntegerProperty DELAY = BlockStateProperties.DELAY;
+
+    public InvertedRepeater(Properties properties) {
+        super(properties);
+        this.registerDefaultState(
+                this.stateDefinition
+                        .any()
+                        .setValue(FACING, Direction.NORTH)
+                        .setValue(DELAY, 1)
+                        .setValue(POWERED, false));
+    }
+
+    public InvertedRepeater() {
+        this(Properties.of());
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
+            BlockHitResult hitResult) {
+        if (!player.getAbilities().mayBuild) {
+            return InteractionResult.PASS;
+        } else {
+            level.setBlock(pos, state.cycle(DELAY), 3);
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, POWERED, DELAY);
+    }
+
+    @Override
+    protected MapCodec<? extends DiodeBlock> codec() {
+        return simpleCodec(InvertedRepeater::new);
+    }
+
+    @Override
+    protected boolean sideInputDiodesOnly() {
+        return true;
+    }
+
+    @Override
+    protected int getDelay(BlockState state) {
+        return state.getValue(DELAY) * 2;
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, BlockGetter world, BlockPos pos, Direction side) {
+        return state.getValue(FACING).equals(side) ||
+                state.getValue(FACING).getOpposite().equals(side);
+    }
+
+    @Override
+    protected boolean shouldTurnOn(Level level, BlockPos pos, BlockState state) {
+        return !(getInputSignal(level, pos, state) > 0);
+
+    }
+
+}
