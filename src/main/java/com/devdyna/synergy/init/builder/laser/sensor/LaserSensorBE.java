9f150c6275ec596504a5666b9c5f923a2bf93a5d
DevDyna <andrearisorti2003@gmail.com>
2025-11-30 16:17:35 +0100

    removed energy capability to laser sensor

--- src/main/java/com/devdyna/synergy/init/builder/laser/sensor/LaserSensorBE.java
+++ src/main/java/com/devdyna/synergy/init/builder/laser/sensor/LaserSensorBE.java
@@ -1,29 +1,20 @@
 package com.devdyna.synergy.init.builder.laser.sensor;
 
-import java.util.*;
+import java.util.List;
 
-import com.devdyna.synergy.api.beLogic.EnergyProvider;
 import com.devdyna.synergy.api.coreBE.be.TickingBE;
 import com.devdyna.synergy.init.types.zBlockEntities;
-import com.devdyna.synergy.init.types.zHandlers;
 import com.devdyna.synergy.utils.LevelUtil;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
-import net.neoforged.neoforge.energy.EnergyStorage;
-import net.neoforged.neoforge.energy.IEnergyStorage;
 
 @SuppressWarnings("null")
-public class LaserSensorBE extends TickingBE implements EnergyProvider {
-
-    private final Map<Direction, BlockCapabilityCache<IEnergyStorage, Direction>> cache = new HashMap<>();
+public class LaserSensorBE extends TickingBE {
 
     public LaserSensorBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
@@ -42,13 +33,9 @@ public class LaserSensorBE extends TickingBE implements EnergyProvider {
 
         level.setBlockAndUpdate(getBlockPos(), getBlockState().setValue(BlockStateProperties.ENABLED, tick > 0));
 
-        if (tick > 0) {
-            if (tick % 4 == 0)
-                increaseFE(1, false);
+        if (tick > 0)
             tick--;
-        }
 
-        providePowerAdjacent(level, getBlockPos(), cache, getStoredFE());
     }
 
     @Override
@@ -59,23 +46,14 @@ public class LaserSensorBE extends TickingBE implements EnergyProvider {
                         getBlockPos().getY() - 0.5, getBlockPos().getZ(), true, 1);
     }
 
+    public boolean canSensorHandle(Direction dir, BlockState state) {
+        var inverted = state.getValue(BlockStateProperties.INVERTED);
+        var dirs = inverted ? List.of(Direction.EAST, Direction.WEST) : List.of(Direction.NORTH, Direction.SOUTH);
+        return dirs.contains(dir);
+    }
+
     public void setActive() {
         tick = MAX_TIMER_COOLDOWN;
-    }
-
-    @Override
-    public ContainerData getContainerData() {
-        return new SimpleContainerData(getMaxFE());
-    }
-
-    @Override
-    public EnergyStorage getCapEnergy() {
-        return getData(zHandlers.ENERGY_STORAGE);
-    }
-
-    @Override
-    public int MaxFE() {
-        return 1_000;
     }
 
 }
