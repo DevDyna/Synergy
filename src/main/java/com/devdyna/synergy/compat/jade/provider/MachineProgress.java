17374c95d25ab252c9c24cdfb909c7baee43bac4
DevDyna <andrearisorti2003@gmail.com>
2025-12-13 14:26:15 +0100

    Update MachineProgress.java

--- src/main/java/com/devdyna/synergy/compat/jade/provider/MachineProgress.java
+++ src/main/java/com/devdyna/synergy/compat/jade/provider/MachineProgress.java
@@ -1,9 +1,11 @@
 package com.devdyna.synergy.compat.jade.provider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.devdyna.synergy.zStatic;
 import com.devdyna.synergy.init.machine.core.BaseMachineBE;
+import com.devdyna.synergy.init.machine.core.SecondaryMachineResult;
 import com.devdyna.synergy.utils.x;
 
 import net.minecraft.network.RegistryFriendlyByteBuf;
@@ -26,25 +28,35 @@ public enum MachineProgress
     // TODO remove default item capability tooltip
 
     Data data = decodeFromData(accessor).orElse(null);
-    if (data == null) {
+    if (data == null)
       return;
-    }
+
     IElementHelper helper = IElementHelper.get();
     tooltip.add(helper.item(data.inventory.get(0)));
     tooltip.append(helper.spacer(4, 0));
     tooltip.append(helper.progress((float) data.progress / data.total).translate(new Vec2(-2, 0)));
     tooltip.append(helper.item(data.inventory.get(1)));
-    tooltip.append(helper.item(data.inventory.get(2)));
+    if (data.hasSecondary)
+      tooltip.append(helper.item(data.inventory.get(2)));
   }
 
   @Override
   public Data streamData(BlockAccessor accessor) {
     BaseMachineBE machineBE = (BaseMachineBE) accessor.getBlockEntity();
-    var storage = machineBE.getStorage();
+
+    List<ItemStack> slots = new ArrayList<>();
+    slots.add(machineBE.getInput());
+    slots.add(machineBE.getOutput());
+
+    if (machineBE instanceof SecondaryMachineResult r) {
+      slots.add(r.getSecondary());
+    }
+
     return new Data(
         machineBE.getProgress(),
         machineBE.getMaxProgress(),
-        List.of(storage.getStackInSlot(0), storage.getStackInSlot(1), storage.getStackInSlot(2)));
+        slots,
+        machineBE instanceof SecondaryMachineResult);
   }
 
   @Override
@@ -52,14 +64,12 @@ public enum MachineProgress
     return Data.STREAM_CODEC;
   }
 
-  public record Data(int progress, int total, List<ItemStack> inventory) {
+  public record Data(int progress, int total, List<ItemStack> inventory, boolean hasSecondary) {
     public static final StreamCodec<RegistryFriendlyByteBuf, Data> STREAM_CODEC = StreamCodec.composite(
-        ByteBufCodecs.VAR_INT,
-        Data::progress,
-        ByteBufCodecs.VAR_INT,
-        Data::total,
-        ItemStack.OPTIONAL_LIST_STREAM_CODEC,
-        Data::inventory,
+        ByteBufCodecs.VAR_INT, Data::progress,
+        ByteBufCodecs.VAR_INT, Data::total,
+        ItemStack.OPTIONAL_LIST_STREAM_CODEC, Data::inventory,
+        ByteBufCodecs.BOOL, Data::hasSecondary,
         Data::new);
   }
 
