41e38a819294ef99471b3e2ac648840427b7f7cb
DevDyna <andrearisorti2003@gmail.com>
2025-12-13 14:26:03 +0100

    Create SecondaryMachineResult.java

---
+++ src/main/java/com/devdyna/synergy/init/machine/core/SecondaryMachineResult.java
@@ -0,0 +1,15 @@
+package com.devdyna.synergy.init.machine.core;
+
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.items.ItemStackHandler;
+
+public interface SecondaryMachineResult {
+    public static final int OUTPUT_SECONDARY_SLOT = 2;
+
+    abstract ItemStackHandler getStorage();
+
+    public default ItemStack getSecondary() {
+        return getStorage().getStackInSlot(OUTPUT_SECONDARY_SLOT);
+    }
+
+}
