8b34883ba88e34b44712c9e83f3e1264c0835165
DevDyna <andrearisorti2003@gmail.com>
2025-12-13 01:10:44 +0100

    jade compat to show progress

--- src/main/java/com/devdyna/synergy/compat/jade/JadePlugin.java
+++ src/main/java/com/devdyna/synergy/compat/jade/JadePlugin.java
@@ -2,8 +2,11 @@ package com.devdyna.synergy.compat.jade;
 
 import com.devdyna.synergy.compat.jade.provider.LaserMachineGunProvider;
 import com.devdyna.synergy.compat.jade.provider.LaserRotorProvider;
+import com.devdyna.synergy.compat.jade.provider.MachineProgress;
 import com.devdyna.synergy.init.builder.laser.dynamo.LaserRotorBlock;
 import com.devdyna.synergy.init.builder.laser.machine_gun.LaserMachineBlock;
+import com.devdyna.synergy.init.machine.core.BaseMachineBlock;
+
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
@@ -16,11 +19,13 @@ public class JadePlugin implements IWailaPlugin {
     public void registerClient(IWailaClientRegistration registration) {
         registration.registerBlockComponent(LaserMachineGunProvider.INSTANCE, LaserMachineBlock.class);
         registration.registerBlockComponent(LaserRotorProvider.INSTANCE, LaserRotorBlock.class);
+        registration.registerBlockComponent(MachineProgress.INSTANCE, BaseMachineBlock.class);
     }
 
     @Override
     public void register(IWailaCommonRegistration registration) {
         registration.registerBlockDataProvider(LaserMachineGunProvider.INSTANCE, LaserMachineBlock.class);
         registration.registerBlockDataProvider(LaserRotorProvider.INSTANCE, LaserRotorBlock.class);
+        registration.registerBlockDataProvider(MachineProgress.INSTANCE, BaseMachineBlock.class);
     }
 }
---
+++ src/main/java/com/devdyna/synergy/compat/jade/provider/MachineProgress.java
@@ -0,0 +1,71 @@
+package com.devdyna.synergy.compat.jade.provider;
+
+import java.util.List;
+
+import com.devdyna.synergy.zStatic;
+import com.devdyna.synergy.init.machine.core.BaseMachineBE;
+import com.devdyna.synergy.utils.x;
+
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec2;
+import snownee.jade.api.*;
+import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IElementHelper;
+
+public enum MachineProgress
+    implements IBlockComponentProvider, StreamServerDataProvider<BlockAccessor, MachineProgress.Data> {
+  INSTANCE;
+
+  @Override
+  public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+
+    // TODO remove default item capability tooltip
+
+    Data data = decodeFromData(accessor).orElse(null);
+    if (data == null) {
+      return;
+    }
+    IElementHelper helper = IElementHelper.get();
+    tooltip.add(helper.item(data.inventory.get(0)));
+    tooltip.append(helper.spacer(4, 0));
+    tooltip.append(helper.progress((float) data.progress / data.total).translate(new Vec2(-2, 0)));
+    tooltip.append(helper.item(data.inventory.get(1)));
+    tooltip.append(helper.item(data.inventory.get(2)));
+  }
+
+  @Override
+  public Data streamData(BlockAccessor accessor) {
+    BaseMachineBE machineBE = (BaseMachineBE) accessor.getBlockEntity();
+    var storage = machineBE.getStorage();
+    return new Data(
+        machineBE.getProgress(),
+        machineBE.getMaxProgress(),
+        List.of(storage.getStackInSlot(0), storage.getStackInSlot(1), storage.getStackInSlot(2)));
+  }
+
+  @Override
+  public StreamCodec<RegistryFriendlyByteBuf, Data> streamCodec() {
+    return Data.STREAM_CODEC;
+  }
+
+  public record Data(int progress, int total, List<ItemStack> inventory) {
+    public static final StreamCodec<RegistryFriendlyByteBuf, Data> STREAM_CODEC = StreamCodec.composite(
+        ByteBufCodecs.VAR_INT,
+        Data::progress,
+        ByteBufCodecs.VAR_INT,
+        Data::total,
+        ItemStack.OPTIONAL_LIST_STREAM_CODEC,
+        Data::inventory,
+        Data::new);
+  }
+
+  @Override
+  public ResourceLocation getUid() {
+    return x.rl(zStatic.Machines.TYPE);
+  }
+
+}
