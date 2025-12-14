e3162c5e27af073289fc8d633d32fd4d25d658c9
DevDyna <andrearisorti2003@gmail.com>
2025-12-14 16:06:38 +0100

    Update ItemStorageBlock.java

--- src/main/java/com/devdyna/synergy/api/beLogic/ItemStorageBlock.java
+++ src/main/java/com/devdyna/synergy/api/beLogic/ItemStorageBlock.java
@@ -2,6 +2,10 @@ package com.devdyna.synergy.api.beLogic;
 
 import net.neoforged.neoforge.items.ItemStackHandler;
 
+/**
+ * Simple ItemStackHandler<br/><br/>
+ * Useful to create chests or single storage IO
+ */
 public interface ItemStorageBlock {
 
     ItemStackHandler getStorage();
