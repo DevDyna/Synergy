5e8258a562ba6fb8750e9bb67585ef25305839ed
DevDyna <andrearisorti2003@gmail.com>
2025-12-26 23:33:34 +0100

    Update DryableBricksRecipe.java

--- src/main/java/com/devdyna/synergy/common/recipes/type/DryableBricksRecipe.java
+++ src/main/java/com/devdyna/synergy/common/recipes/type/DryableBricksRecipe.java
@@ -10,7 +10,10 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.PlacementInfo;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeBookCategories;
+import net.minecraft.world.item.crafting.RecipeBookCategory;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
@@ -41,7 +44,7 @@ public class DryableBricksRecipe implements Recipe<MonoItemInput> {
         return false;
     }
 
-    public RecipeType<?> getType() {
+    public RecipeType<DryableBricksRecipe> getType() {
         return zRecipeTypes.DRYABLE_BRICKS.getType();
     }
 
@@ -50,17 +53,12 @@ public class DryableBricksRecipe implements Recipe<MonoItemInput> {
     }
 
     @Override
-    public RecipeSerializer<?> getSerializer() {
+    public RecipeSerializer<DryableBricksRecipe> getSerializer() {
         return zRecipeTypes.DRYABLE_BRICKS.getSerializer();
     }
 
     public NonNullList<Ingredient> getIngredients() {
         return NonNullList.copyOf(List.of(input));
-    }
-
-    @Override
-    public ItemStack getResultItem(HolderLookup.Provider registryAccess) {
-        return output;
     }
 
     public Ingredient getInput() {
@@ -73,5 +71,15 @@ public class DryableBricksRecipe implements Recipe<MonoItemInput> {
 
     public BlockState getBlock() {
         return block;
+    }
+
+    @Override
+    public PlacementInfo placementInfo() {
+        return PlacementInfo.NOT_PLACEABLE;
+    }
+
+    @Override
+    public RecipeBookCategory recipeBookCategory() {
+        return RecipeBookCategories.CRAFTING_BUILDING_BLOCKS;
     }
 }
