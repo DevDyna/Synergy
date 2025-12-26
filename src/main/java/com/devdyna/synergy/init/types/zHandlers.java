23d7be584fd0d7369fcc2d55fe9faa4a251d21bd
DevDyna <andrearisorti2003@gmail.com>
2025-12-26 23:05:53 +0100

    Update zHandlers.java

--- src/main/java/com/devdyna/synergy/init/types/zHandlers.java
+++ src/main/java/com/devdyna/synergy/init/types/zHandlers.java
@@ -12,6 +12,8 @@ import net.neoforged.neoforge.energy.EnergyStorage;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries.Keys;
+import net.neoforged.neoforge.transfer.energy.EnergyHandler;
+import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 
 public class zHandlers {
     public static void register(IEventBus bus) {
@@ -26,25 +28,25 @@ public class zHandlers {
 
     // ---------------------------------------------------------------------------------------//
 
-    public static final Supplier<AttachmentType<EnergyStorage>> ENERGY_STORAGE = zHandler.register(
+    public static final Supplier<AttachmentType<EnergyHandler>> ENERGY_STORAGE = zHandler.register(
             "energy_storage",
             () -> AttachmentType.serializable(h -> (h instanceof EnergyBlock be)
-                    ? new EnergyStorage(be.MaxFE())
+                    ? new EnergyHandler((be).MaxFE())
                     : null).build());
 
-    public static final Supplier<AttachmentType<ItemStackHandler>> ITEM_STORAGE = zHandler.register(
+    public static final Supplier<AttachmentType<ItemStacksResourceHandler>> ITEM_STORAGE = zHandler.register(
             "item_storage", () -> AttachmentType.serializable(h -> {
                 if (h instanceof ItemStorageBlock be)
-                    return new ItemStackHandler(be.MachineSlots());
-                return new ItemStackHandler(1);
+                    return new ItemStacksResourceHandler(be.MachineSlots());
+                return new ItemStacksResourceHandler(1);
             }).build());
 
 
-public static final Supplier<AttachmentType<ItemStackHandler>> MACHINE_HANDLER = zHandler.register(
+public static final Supplier<AttachmentType<ItemStacksResourceHandler>> MACHINE_HANDLER = zHandler.register(
             "items", () -> AttachmentType.serializable(h -> {
                 if (h instanceof MachineItemAutomation be)
-                    return new ItemStackHandler(be.getMachineSlots());
-                return new ItemStackHandler(1);
+                    return new ItemStacksResourceHandler(be.getMachineSlots());
+                return new ItemStacksResourceHandler(1);
             }).build());
 
               
