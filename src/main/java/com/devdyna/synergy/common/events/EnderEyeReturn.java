b1c91dc4f259528da5bd66f3fdf2ab7750811a5c
DevDyna <andrearisorti2003@gmail.com>
2025-12-27 16:13:52 +0100

    end portal frame event

---
+++ src/main/java/com/devdyna/synergy/common/events/EnderEyeReturn.java
@@ -0,0 +1,24 @@
+package com.devdyna.synergy.common.events;
+
+import com.devdyna.synergy.api.utils.x;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.*;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.items.ItemHandlerHelper;
+
+public class EnderEyeReturn {
+    @SubscribeEvent
+    public static void endFrameEncaseEvent(PlayerInteractEvent.RightClickBlock event) {
+        var pos = event.getPos();
+        var level = event.getLevel();
+        var player = event.getEntity();
+        var state = level.getBlockState(pos);
+
+        if (state.is(Blocks.END_PORTAL_FRAME) && state.getValue(EndPortalFrameBlock.HAS_EYE)) {
+            ItemHandlerHelper.giveItemToPlayer(player, x.item(Items.ENDER_EYE));
+            level.setBlockAndUpdate(pos, state.setValue(EndPortalFrameBlock.HAS_EYE, false));
+        }
+
+    }
+}
