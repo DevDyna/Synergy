5e0d9cef8c511f3a5ca10dd883fb09e672b1d3e6
DevDyna <andrearisorti2003@gmail.com>
2026-01-19 17:14:18 +0100

    Create RecipeUtils.java

---
+++ src/main/java/com/devdyna/synergy/api/utils/RecipeUtils.java
@@ -0,0 +1,36 @@
+package com.devdyna.synergy.api.utils;
+
+import java.util.List;
+
+import com.devdyna.synergy.api.MachineType;
+import com.devdyna.synergy.api.zRecipe;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeInput;
+import net.minecraft.world.item.crafting.RecipeType;
+
+@SuppressWarnings("null")
+public class RecipeUtils {
+
+    public static <T extends Recipe<I>, I extends RecipeInput> List<RecipeHolder<T>> getRecipes(RecipeType<T> r) {
+        return Minecraft.getInstance().level.getRecipeManager().getAllRecipesFor(r);
+    }
+
+    public static <T extends Recipe<I>, I extends RecipeInput> List<RecipeHolder<T>> getRecipes(zRecipe<T> r) {
+        return getRecipes(r.getType());
+    }
+
+    public static <T extends Recipe<I>, I extends RecipeInput> List<RecipeHolder<T>> getRecipes(
+            MachineType<?, ?, ?, T> r) {
+        return getRecipes(r.recipe().getType());
+    }
+
+    public static <T extends Recipe<I>, I extends RecipeInput> void registerCategory(IRecipeRegistration i,
+            zRecipe<T> r) {
+        i.addRecipes(mezz.jei.api.recipe.RecipeType.createFromVanilla(r.getType()),
+                RecipeUtils.getRecipes(r.getType()));
+    }
+
+}
