500527ea16102829b2c5f5df94c266275e948a4e
DevDyna <andrearisorti2003@gmail.com>
2026-02-28 14:28:07 +0100

    reworked base screen to contain default values intend of abstract values

--- src/main/java/com/devdyna/synergy/api/gui/BaseScreen.java
+++ src/main/java/com/devdyna/synergy/api/gui/BaseScreen.java
@@ -6,10 +6,12 @@ import javax.annotation.Nullable;
 
 import com.devdyna.synergy.api.utils.ColorUtil;
 
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Inventory;
 
 @SuppressWarnings("null")
@@ -21,11 +23,17 @@ public abstract class BaseScreen<T extends BaseMenu> extends AbstractContainerSc
 
     protected abstract ResourceLocation background();
 
-    protected abstract @Nullable ResourceLocation arrow();
+    protected @Nullable ResourceLocation arrow(){
+        return null;
+    }
 
-    protected abstract boolean whenAnimateArrow();
+    protected boolean whenAnimateArrow(){
+        return false;
+    }
 
-    protected abstract int getScaledArrowProgress();
+    protected int getScaledArrowProgress(){
+        return 0;
+    }
 
     protected final Color defaultToolTipColor = ColorUtil.color(64, 64, 64);
 
@@ -45,5 +53,10 @@ public abstract class BaseScreen<T extends BaseMenu> extends AbstractContainerSc
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
         renderTooltip(pGuiGraphics, pMouseX, pMouseY);
     }
+
+    public void drawCenteredString(GuiGraphics pGuiGraphics,Font font, Component text, int x, int y, int color,boolean dropShadow) {
+      FormattedCharSequence formattedcharsequence = text.getVisualOrderText();
+       pGuiGraphics.drawString(font, formattedcharsequence, x - font.width(formattedcharsequence) / 2, y, color,dropShadow);
+   }
 
 }
