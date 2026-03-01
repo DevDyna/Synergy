00da44f3620774acf6a5749eb4656281677cb35c
DevDyna <andrearisorti2003@gmail.com>
2026-03-01 20:44:12 +0100

    Update ArrayUtils.java

--- src/main/java/com/devdyna/synergy/api/utils/ArrayUtils.java
+++ src/main/java/com/devdyna/synergy/api/utils/ArrayUtils.java
@@ -1,6 +1,8 @@
 package com.devdyna.synergy.api.utils;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Stream;
 
 public class ArrayUtils {
@@ -25,6 +27,15 @@ public class ArrayUtils {
 
     public static <T> List<T> concat(Stream<T> a1, T a2) {
         return concat(a2, a1);
+    }
+
+    /**
+     * @param l initial array
+     * @param m map to convert things
+     * @param b builder used to create the array ex. Integer[]::new
+     */
+    public static <T, R> R[] map(T[] l, Function<? super T, ? extends R> m, IntFunction<R[]> b) {
+        return Arrays.asList(l).stream().map(m).toArray(b::apply);
     }
 
 }
