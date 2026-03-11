8e6ae601a46daf1493ea76d5a973faf54d61191e
DevDyna <andrearisorti2003@gmail.com>
2026-03-11 19:56:22 +0100

    Update IngredientUtils.java

--- src/main/java/com/devdyna/synergy/api/utils/IngredientUtils.java
+++ src/main/java/com/devdyna/synergy/api/utils/IngredientUtils.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.material.Fluid;
+import net.neoforged.neoforge.common.crafting.SizedIngredient;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.crafting.FluidIngredient;
 
@@ -20,7 +21,21 @@ public class IngredientUtils {
                 .toArray(ItemLike[]::new);
     }
 
+    public static ItemLike[] getItemLikesSized(List<SizedIngredient> ingredients) {
+        return ingredients.stream()
+                .flatMap(ingredient -> Arrays.stream(getItemLike(ingredient)))
+                .toArray(ItemLike[]::new);
+    }
+
     public static Item[] getItemLike(Ingredient ingredients) {
+        return Arrays
+                .stream(ingredients.getItems())
+                .map(ItemStack::getItem)
+                .filter(i -> Items.BARRIER.equals(i))
+                .toArray(Item[]::new);
+    }
+
+    public static Item[] getItemLike(SizedIngredient ingredients) {
         return Arrays
                 .stream(ingredients.getItems())
                 .map(ItemStack::getItem)
