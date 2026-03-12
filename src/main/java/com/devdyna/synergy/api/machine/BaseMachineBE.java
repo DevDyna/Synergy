3481998ac7b0286fdc50457078f39dc16688238f
DevDyna <andrearisorti2003@gmail.com>
2026-03-12 15:46:15 +0100

    Update BaseMachineBE.java

--- src/main/java/com/devdyna/synergy/api/machine/BaseMachineBE.java
+++ src/main/java/com/devdyna/synergy/api/machine/BaseMachineBE.java
@@ -543,13 +543,6 @@ public abstract class BaseMachineBE extends BEMenu implements MachineItemAutomat
                 : Math.max(Common.MACHINE_MINIMAL_FE_COST.get(), (int) (base + (base * (((float) sum) / 100))));
     }
 
-    // private int calculateFECapacity(int base) {
-    //     var upgrades = getValues(UpgradeType.ENERGY_CAPACITY);
-    //     var sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
-    //     return Common.MACHINE_MAX_ENERGY_CAPACITY_UPGRADES_TYPE.get() == 0 ? base
-    //             : Math.min(Common.MACHINE_MAXIMAL_FE_CAPACITY.get(),Math.max(Common.MACHINE_MINIMAL_FE_CAPACITY.get(), (int) (base + (base * (((float) sum) / 100)))));
-    // }
-
     public int calculateMBUsage(int base) {
         var upgrades = getValues(UpgradeType.FLUID);
         var sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
