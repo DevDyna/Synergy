b455b31bb1a3d3fbf06f1729d2f0d2e021c1269b
DevDyna <andrearisorti2003@gmail.com>
2026-03-22 18:49:53 +0100

    initialized ponder plugin

---
+++ src/main/java/com/devdyna/synergy/compat/ponder/Plugin.java
@@ -0,0 +1,39 @@
+package com.devdyna.synergy.compat.ponder;
+
+import static com.devdyna.synergy.Main.ID;
+
+import com.devdyna.synergy.api.utils.x;
+import com.devdyna.synergy.init.types.zBlocks;
+
+import net.createmod.ponder.api.registration.PonderPlugin;
+import net.createmod.ponder.api.registration.PonderSceneRegistrationHelper;
+import net.createmod.ponder.api.registration.PonderTagRegistrationHelper;
+import net.minecraft.resources.ResourceLocation;
+
+@SuppressWarnings("null")
+public class Plugin implements PonderPlugin {
+
+    @Override
+    public String getModId() {
+        return ID;
+    }
+
+    @Override
+    public void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> h) {
+
+
+        // h.addStoryBoard(zBlocks.HARVESTER.getId(), x.rl(""), null);
+
+        //node
+        //redstone repeaters
+        //sprinker
+        //harvester
+
+    }
+
+    @Override
+    public void registerTags(PonderTagRegistrationHelper<ResourceLocation> h) {
+
+    }
+
+}
