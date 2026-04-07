231ee877483bc64b259446e8ceedf50900216b7a
DevDyna <andrearisorti2003@gmail.com>
2026-04-07 21:39:47 +0200

    Update ModAddonUtil.java

--- src/main/java/com/devdyna/synergy/api/utils/ModAddonUtil.java
+++ src/main/java/com/devdyna/synergy/api/utils/ModAddonUtil.java
@@ -1,9 +1,15 @@
 package com.devdyna.synergy.api.utils;
 
 import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.common.conditions.ICondition;
+import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
 
 public class ModAddonUtil {
     public static boolean checkMod(String s) {
         return ModList.get().isLoaded(s);
+    }
+
+    public static ICondition hasMod(String s) {
+        return new ModLoadedCondition(s);
     }
 }
