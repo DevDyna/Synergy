25e044c49b77e2903cba3ef4922b3fbdb7082df7
DevDyna <andrearisorti2003@gmail.com>
2026-04-07 19:15:06 +0200

    Update BiBool.java

--- src/main/java/com/devdyna/synergy/api/utils/BiBool.java
+++ src/main/java/com/devdyna/synergy/api/utils/BiBool.java
@@ -1,5 +1,12 @@
 package com.devdyna.synergy.api.utils;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+
 public class BiBool {
     private Boolean value1;
     private Boolean value2;
@@ -28,5 +35,16 @@ public class BiBool {
     public static BiBool of() {
         return new BiBool(false, false);
     }
+
+    public static final Codec<BiBool> CODEC = RecordCodecBuilder.create(inst -> inst.group(
+            Codec.BOOL.fieldOf("value_a").forGetter(BiBool::get1),
+            Codec.BOOL.fieldOf("value_b").forGetter(BiBool::get2))
+            .apply(inst, BiBool::new));
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, BiBool> STREAM_CODEC = StreamCodec
+            .composite(
+                    ByteBufCodecs.BOOL, BiBool::get1,
+                    ByteBufCodecs.BOOL, BiBool::get2,
+                    BiBool::new);
 
 }
