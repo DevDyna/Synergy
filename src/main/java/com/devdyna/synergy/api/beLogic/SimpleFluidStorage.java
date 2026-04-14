492c60947e5c43f60473a239c205a23ff430fac2
DevDyna <andrearisorti2003@gmail.com>
2026-04-15 00:27:37 +0200

    Update SimpleFluidStorage.java

--- src/main/java/com/devdyna/synergy/api/beLogic/SimpleFluidStorage.java
+++ src/main/java/com/devdyna/synergy/api/beLogic/SimpleFluidStorage.java
@@ -1,11 +1,18 @@
 package com.devdyna.synergy.api.beLogic;
 
-import com.devdyna.synergy.api.FluidStorageTank;
+import com.devdyna.synergy.api.fluidtanks.FluidStorageTank;
+import com.devdyna.synergy.api.utils.x;
+
+import net.minecraft.world.item.ItemStack;
 
 public interface SimpleFluidStorage {
 
     FluidStorageTank getFluidStorage();
 
     int getFluidCapacity();
+
+    default ItemStack getAsBucket() {
+        return x.item(getFluidStorage().getFluid().getFluid().getBucket());
+    }
 
 }
